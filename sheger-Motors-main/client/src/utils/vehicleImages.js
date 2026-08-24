439c4e037b02cd3f2a7edb4e1af8c524cfcc0b24
Atinaf Bedasa <142307958+atikoo67@users.noreply.github.com>
2026-08-25 00:27:46 +0300

    delete a file uploaded by mistakenly

--- sheger-Motors-main/client/src/utils/vehicleImages.js
+++
@@ -1,8 +0,0 @@
-export function getVehicleImages(vehicle) {
-  if (!vehicle || !Array.isArray(vehicle.images)) return [];
-  return vehicle.images.filter((image) => image?.image_url);
-}
-
-export function getPrimaryImageUrl(vehicle) {
-  return getVehicleImages(vehicle)[0]?.image_url || null;
-}
